1a4aaccc0a81f9d25900b51472a3948639c12d27
MOONWUD <moonqazwsx@naver.com>
2024-06-05 11:40:40 +0900

    240605 가로스크롤 변경

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -3,19 +3,20 @@ history.scrollRestoration = "manual"; // 새로고침 위치 기억
 /*===== 클릭시 이미지 생성  =====*/
 
 /*===== main  =====*/
-/*=== text ===*/
+/*=== title ===*/
+/* 첫번째 줄 */
 var textWrapper = document.querySelector('.ml1');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: 2})
+anime.timeline({ loop: 2 })
   .add({
     targets: '.ml1 .letter',
-    scale: [4,1],
-    opacity: [0,1],
+    scale: [4, 1],
+    opacity: [0, 1],
     translateZ: 0,
     easing: "easeOutExpo",
     duration: 950,
-    delay: (el, i) => 70*i
+    delay: (el, i) => 70 * i
   }).add({
     targets: '.ml1',
     opacity: 1,
@@ -24,18 +25,19 @@ anime.timeline({loop: 2})
     delay: 1000
   });
 
+/* 두번째 줄 */
 var textWrapper = document.querySelector('.ml2');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: 2})
+anime.timeline({ loop: 2 })
   .add({
     targets: '.ml2 .letter',
-    scale: [4,1],
-    opacity: [0,1],
+    scale: [4, 1],
+    opacity: [0, 1],
     translateZ: 0,
     easing: "easeOutExpo",
     duration: 950,
-    delay: (el, i) => 70*i + 500
+    delay: (el, i) => 70 * i + 500
   }).add({
     targets: '.ml2',
     opacity: 1,
@@ -44,7 +46,7 @@ anime.timeline({loop: 2})
     delay: 1000
   });
 
-/*=== box ===*/
+/*=== background ===*/
 gsap.to(".box1", {
   scrollTrigger: {
     trigger: '.box1',//객체기준범위
@@ -60,8 +62,7 @@ gsap.to(".box1", {
 });
 
 /*===== about =====*/
-/*=== drop ===*/
-
+/*=== scale text ===*/
 gsap.to(".drop .contentsWrapper .text-wrapper", {
   scrollTrigger: {
     trigger: '.drop',//객체기준범위
@@ -69,7 +70,7 @@ gsap.to(".drop .contentsWrapper .text-wrapper", {
     end: "80% 80%",//끝 지점
     // end: "+=500"//시작 부분부터 500px까지 스크롤 한 후종료
     scrub: 1,//부드러운 스크러빙
-    markers: true,//개발가이드선
+    //markers: true,개발가이드선
   },
   scale: 1,
   borderRadius: 0,
@@ -77,6 +78,7 @@ gsap.to(".drop .contentsWrapper .text-wrapper", {
   opacity: 1,
 });
 
+/*=== glitters ===*/
 $(window).on('scroll', () => {
   let dropHt = $('.drop .contentsWrapper .text-wrapper').offset().top - 350;
   if (scrollY > dropHt) {
@@ -88,8 +90,6 @@ $(window).on('scroll', () => {
 })
 
 /*===== skill & tool =====*/
-/*=== title ===*/
-/*=== 날라오는 박스 ===*/
 $(window).on('scroll', () => {
   let skillHt = $('.skillTool').offset().top - 400;
   if (scrollY > skillHt) {
@@ -101,12 +101,21 @@ $(window).on('scroll', () => {
 })
 
 /*===== project =====*/
-$(window).on('scroll', () => {
-  let projectHt = $('.project').offset().top - 400;
-  if (scrollY > projectHt) {
-    $('.project').addClass('show');
-    
-  } else {
-    $('.project').removeClass('show');
+/*=== title ===*/
+/*=== 가로스크롤 ===*/
+
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".projectContainer");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".project",
+    pin: true,
+    scrub: 1,
+    // snap: 1 / (sections.length - 1),s
+    end: () => "+=" + document.querySelector(".project").offsetWidth
   }
-})
+});
