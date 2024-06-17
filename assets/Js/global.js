922cc6eba8a34c9a4d94aa4001f6de9c7a5537fd
MOONWUD <moonqazwsx@naver.com>
2024-06-17 10:37:19 +0900

    240617 피드백 수정

--- assets/Js/global.js
+++ assets/Js/global.js
@@ -1,28 +1,3 @@
-/*===== mouse emoji =====*/
-const emoji = document.querySelector('.logo-emoji');
-$('.logo-emoji');
-
-let mouseX = 0;
-let mouseY = 0;
-
-addEventListener('mousemove', (e) => {
-  mouseX = e.clientX - 10;
-  mouseY = e.clientY - 10;
-})
-let emojiX = 0;
-let emojiY = 0;
-
-const emojiMov = () => {
-  requestAnimationFrame(emojiMov);
-
-  emojiX += (mouseX - emojiX) * 0.1;
-  emojiY += (mouseY - emojiY) * 0.1;
-
-  emoji.style.transform = `translate(${emojiX}px, ${emojiY}px)`;
-}
-
-emojiMov();
-
 /*===== 눈알 굴러가는 이벤트 =====*/
 var eyeball = $(".pupil");
 
--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,5 +1,30 @@
 history.scrollRestoration = "manual"; // 새로고침 위치 기억
 
+/*===== mouse emoji =====*/
+const emoji = document.querySelector('.logo-emoji');
+$('.logo-emoji');
+
+let mouseX = 0;
+let mouseY = 0;
+
+addEventListener('mousemove', (e) => {
+  mouseX = e.clientX - 10;
+  mouseY = e.clientY - 10;
+})
+let emojiX = 0;
+let emojiY = 0;
+
+const emojiMov = () => {
+  requestAnimationFrame(emojiMov);
+
+  emojiX += (mouseX - emojiX) * 0.1;
+  emojiY += (mouseY - emojiY) * 0.1;
+
+  emoji.style.transform = `translate(${emojiX}px, ${emojiY}px)`;
+}
+
+emojiMov();
+
 /*===== header =====*/
 let prevScroll = window.scrollY;
 $(window).on('scroll', () => {
